9662c1e164d9c3f8b0dddee40a500c5ed0027e94
Derek A <derek1aslan@gmail.com>
2023-01-21 09:53:56 -0800

    Created custom class for limelight game piece detection, to make it easier to work with

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,21 +1,25 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.apriltag.AprilTag;
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-import java.util.ArrayList;
-
 public class Limelight extends SubsystemBase{
+    public static class GamePiece{
+        public final boolean isCube;
+        public final double xPos;
+        public final double yPos;
+        private GamePiece(boolean isCube, double xPos, double yPos){
+            this.isCube = isCube;
+            this.xPos = xPos;
+            this.yPos = yPos;
+        }
+    }
     private NetworkTable limeLight;
     public Limelight(){
         limeLight = NetworkTableInstance.getDefault().getTable("limelight");
@@ -24,11 +28,11 @@ public class Limelight extends SubsystemBase{
     public boolean visionTargetsFound(){
         return limeLight.getEntry("tv").getDouble(0) != 0; // Key tv is 1 for a vision target found and 0 for no target
     }
-    private double[][] getGamePiecePositions(){
+    private GamePiece[] getGamePiecePositions(){
         double[] rawPositions = limeLight.getEntry("llpython").getDoubleArray(new double[]{});
-        double[][] positions = new double[3][rawPositions.length/3];
+        GamePiece[] positions = new GamePiece[rawPositions.length/3];
         for(int i=0; i<rawPositions.length/3; i++){
-            positions[i] = new double[]{rawPositions[3*i], rawPositions[3*i+1], rawPositions[3*i+2]};
+            positions[i] = new GamePiece(rawPositions[3 * i] == 1, rawPositions[3 * i + 1], rawPositions[3 * i + 2]);
         }
         return positions;
     }
