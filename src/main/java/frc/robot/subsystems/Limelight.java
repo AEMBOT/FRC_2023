9ccc20d6fe79b0aac2af574224b433bf5650b044
Derek A <derek1aslan@gmail.com>
2023-01-28 11:46:54 -0800

    Bugfix

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -161,7 +161,7 @@ public class Limelight extends SubsystemBase{
                 }
                 rawPosition = newRawPosition;
             }
-            position = new Pose3d(new Translation3d(rawPosition[0], rawPosition[1], rawPosition[2]), new Rotation3d(rawPosition[4], rawPosition[6], rawPosition[5])); // Convert to Pose2d for use elsewhere
+            position = new Pose3d(new Translation3d(rawPosition[0], rawPosition[1], rawPosition[2]), new Rotation3d(rawPosition[3], rawPosition[4], rawPosition[5])); // Convert to Pose2d for use elsewhere
             rawPosition = limeLight.getEntry("camtran").getDoubleArray(new double[]{0,0,0,0,0,0}); // Get position (botpose returns a double array, [xpos, ypos, zpos, xrot, yrot, zrot]
             NetworkTableInstance.getDefault().getTable("LimelightTesting").getEntry("rawCamTran").setValue(rawPosition);
             if(rawPosition.length < 6){
