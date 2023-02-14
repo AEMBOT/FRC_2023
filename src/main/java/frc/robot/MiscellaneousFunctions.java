74530809ad12bf34a55769c3f17da0bd0073ab34
AbiralShakya <abiral.shakya6@gmail.com>
2023-02-13 16:29:51 -0800

    inverse math fun

--- src/main/java/frc/robot/MiscellaneousFunctions.java
+++ src/main/java/frc/robot/MiscellaneousFunctions.java
@@ -11,4 +11,14 @@ public class MiscellaneousFunctions{
     public static double ArmAngleToDistance(double radians){
         return Math.sqrt(Math.pow((pivotPointDistance+pivotRadius * Math.cos(radians)),2) + Math.pow((pivotPointHeight+pivotRadius * Math.sin(radians)),2));
     }
+
+    public static double DistanceToArmAngle(double distance){
+        double radians = distance / (2 * Math.PI);
+        double a = - pivotPointDistance * Math.cos(radians);
+        double b =  -pivotPointHeight * Math.sin(radians);
+        double c = Math.sqrt(-Math.pow(pivotPointDistance,2) * Math.pow(Math.sin(radians), 2) + 2 * pivotPointDistance * pivotPointHeight
+        * Math.sin(radians) * Math.cos(radians) - Math.pow(pivotPointHeight,2) * Math.pow(Math.cos(radians),2) + Math.pow(pivotRadius,2));
+        return a + b + c;
+        
+    }
 }
