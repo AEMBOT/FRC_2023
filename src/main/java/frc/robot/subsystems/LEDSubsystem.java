03e331a96d1fdec6a8077ef64111ab9691ca112f
Yasmine <elise.othman@gmail.com>
2023-02-18 15:52:26 -0800

    LED command and subsystem modified

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import java.nio.Buffer;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,60 +10,70 @@ public class LEDSubsystem extends SubsystemBase {
 
     private final AddressableLED m_led = new AddressableLED(9);
     private final AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(30);
+    public boolean rainbowYes = true;
+    //how many pixels before it goes full bright again
+    int DarkPix = 6;
+
+   
+    //declare the red, green, and blue values, and declare the SubtractVal array.
+    int red;
+    int green;
+    int blue;
+    int[] SubtractVal = {0, 0, 0};
 
 
     public LEDSubsystem() {
-
-
         m_led.setLength(m_ledBuffer.getLength());
-
-        //color codes, color is specified after the list initiation. will add input support soon
-
-
-        //how many pixels before it goes full bright again
-        int DarkPix = 6;
-
-        //color codes, color is specified after the array
-        int[] color = {64, 3, 3}; //red
-        //int[] color = {0,0,64}; //blue
-        //int[] color = {64,32,0}; //yellow
-        //int[] color = {64,0,64}; //purple
-
-        //int[] color = {64,36,0}; //orange
-        //int[] color = {0,64,0}; //green
-
-        //last 2 so we have a rainbow if nessecary. IDK why we would need one though. Celebration?
-
-
-        //declare the red, green, and blue values, and declare the SubtractVal array.
-        int red;
-        int green;
-        int blue;
-        int[] SubtractVal = {0, 0, 0};
-
-
-        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-            //set all of the subtraction values to a mapped value
-            SubtractVal[0] = map((i % DarkPix), 0, (DarkPix), 0, color[0]);
-            SubtractVal[1] = map((i % DarkPix), 0, (DarkPix), 0, color[1]);
-            SubtractVal[2] = map((i % DarkPix), 0, (DarkPix), 0, color[2]);
-            //set the modified red, green, and blue values
-            red = Math.round(color[0] - SubtractVal[0]);
-            green = Math.round(color[1] - SubtractVal[1]);
-            blue = Math.round(color[2] - SubtractVal[2]);
-            // Sets the specified LED to the RGB values
-            m_ledBuffer.setRGB(i, red, green, blue);
-        }
-
-        m_led.setData(m_ledBuffer);
         m_led.start();
 
     }
 
+    public void setColor(int[] Color){
+        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+            //set all of the subtraction values to a mapped value
+            SubtractVal[0] = map((i % DarkPix), 0, (DarkPix), 0, Color[0]);
+            SubtractVal[1] = map((i % DarkPix), 0, (DarkPix), 0, Color[1]);
+            SubtractVal[2] = map((i % DarkPix), 0, (DarkPix), 0, Color[2]);
+            //set the modified red, green, and blue values
+            red = Math.round(Color[0] - SubtractVal[0]);
+            green = Math.round(Color[1] - SubtractVal[1]);
+            blue = Math.round(Color[2] - SubtractVal[2]);
+            // Sets the specified LED to the RGB values
+            m_ledBuffer.setRGB(i, red, green, blue);
+        }
+        m_led.setData(m_ledBuffer);
+    }
+
+
+    public void rainbow(){
+        int m_rainbowFirstPixelHue = 2;
+        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+            // Calculate the hue - hue is easier for rainbows because the color
+            // shape is a circle so only one value needs to precess
+            final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+            // Set the value
+            m_ledBuffer.setHSV(i, hue, 255, 128);
+          }
+          // Increase by to make the rainbow "move"
+          m_rainbowFirstPixelHue += 3;
+          // Check bounds
+          m_rainbowFirstPixelHue %= 180;
+        m_led.setData(m_ledBuffer);
+    }
+
+    public void rainbowIsNo(){
+        rainbowYes = false;
+    }
+
+    public void rainbowIsYes(){
+        rainbowYes = true;
+    }
 
     @Override
     public void periodic() {
-
+        if(rainbowYes){
+            rainbow();
+        }
     }
 
     @Override
