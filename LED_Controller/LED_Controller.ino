f7c870be24e1bcc16877aae1882599bcb428575f
Nolan Hergert <nolan.hergert@gmail.com>
2023-03-01 22:01:49 -0800

    Need to test, but here is modulo code

--- LED_Controller/LED_Controller.ino
+++ LED_Controller/LED_Controller.ino
@@ -1,10 +1,13 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN D1	 // input pin Neopixel is attached to
+#define PIN D1  // input pin Neopixel is attached to
+#define PIN2 D5
 
-#define NUMPIXELS      58 // number of neopixels in strip
+#define NUMPIXELS 58  // number of neopixels in strip
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels2 = Adafruit_NeoPixel(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
+
 
 int add = 0;
 
@@ -17,55 +20,64 @@ int storedBlue = 203;
 void setup() {
   // Initialize the NeoPixel library.
   pixels.begin();
-  
+
   Serial.begin(115200);
-  
+
   pinMode(D1, OUTPUT);
+  pinMode(D5, OUTPUT);
+}
+
+int modulo(int a, int n) {
+  int mod = a % n;
+  if (mod < 0) {
+    mod += n;
+  }
+  return mod;
 }
 
 void loop() {
   setColor(add, DarkPix);
   delay(50);
-  add = (add + 1) % DarkPix;
-  }
-    
+  add = modulo(add - 1, DarkPix);
+}
+
 
 void setColor(int add, int DarkPix) {
   if (Serial.available()) {
     int input = Serial.read();
-    if (input == 'p') { //purple
+    if (input == 'p') {  //purple
       storedRed = 128;
       storedGreen = 0;
       storedBlue = 255;
-    } else if (input == 'y') { //yellow
+    } else if (input == 'y') {  //yellow
       storedRed = 255;
       storedGreen = 182;
       storedBlue = 0;
-    } else if (input == 'l') { //AEMLIGHT
+    } else if (input == 'l') {  //AEMLIGHT
       storedRed = 60;
       storedGreen = 112;
       storedBlue = 203;
-    } else if (input == 'd') { //AEMDARK
+    } else if (input == 'd') {  //AEMDARK
       storedRed = 16;
       storedGreen = 84;
       storedBlue = 162;
-    } else if (input == 'r') {//red
+    } else if (input == 'r') {  //red
       storedRed = 255;
       storedGreen = 0;
       storedBlue = 0;
-    } else if (input == 'b') { //blue
+    } else if (input == 'b') {  //blue
       storedRed = 0;
       storedGreen = 0;
       storedBlue = 255;
-    } else if (input == 'g') { //green
-      storedRed = 0; 
+    } else if (input == 'g') {  //green
+      storedRed = 0;
       storedGreen = 255;
       storedBlue = 0;
-    } else if (input == 'o') { //orange
+    } else if (input == 'o') {  //orange
       storedRed = 255;
       storedGreen = 65;
       storedBlue = 0;
-    } else if (input == '0') { // black
+    } else if (input == '0') {  // black
       storedRed = 0;
       storedGreen = 0;
       storedBlue = 0;
@@ -79,18 +91,17 @@ void setColor(int add, int DarkPix) {
   int SubtractValG;
   int SubtractValB;
 
-  for (int i=0; i < NUMPIXELS; i++) {
-    SubtractValR = map(((i+add)%DarkPix),0,(DarkPix),0,storedRed);
-    SubtractValG = map(((i+add)%DarkPix),0,(DarkPix),0,storedGreen);
-    SubtractValB = map(((i+add)%DarkPix),0,(DarkPix),0,storedBlue);
-    red = round(storedRed-SubtractValR);
-    green = round(storedGreen-SubtractValG);
-    blue = round(storedBlue-SubtractValB);
-    pixels.setPixelColor(i, pixels.Color(red,green,blue));
+  for (int i = 0; i < NUMPIXELS; i++) {
+    SubtractValR = map(modulo(i + add, DarkPix), 0, (DarkPix), 0, storedRed);
+    SubtractValG = map(modulo(i + add, DarkPix), 0, (DarkPix), 0, storedGreen);
+    SubtractValB = map(modulo(i + add, DarkPix), 0, (DarkPix), 0, storedBlue);
+    red = round(storedRed - SubtractValR);
+    green = round(storedGreen - SubtractValG);
+    blue = round(storedBlue - SubtractValB);
+    pixels.setPixelColor(i, pixels.Color(red, green, blue));
+    pixels2.setPixelColor(i, pixels2.Color(red, green, blue));
   }
-    // This sends the updated pixel color to the hardware.
+  // This sends the updated pixel color to the hardware.
   pixels.show();
-    
-  
+  pixels2.show();
 }
-
